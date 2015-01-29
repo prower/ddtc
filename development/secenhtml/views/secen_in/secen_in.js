9618f72e7244b0717bcf6b790eb43d460304c7a9
k k <kk@kMacBook-Pro.local>
2015-01-29 17:52:21 +0800

    现场入场页面更新

--- development/secenhtml/views/secen_in/secen_in.js
+++ development/secenhtml/views/secen_in/secen_in.js
@@ -60,7 +60,7 @@ define(['jquery', 'utils', 'ajax'],function($, utils, ajax){
         ,c_getrow:function(data){
             var me = this;
             var row = this.dom.row.clone();
-            row.find('[name=cardid]').html(data.carid).end().find('[name=time]').html(data.orderTime)
+            row.find('[name=cardid]').html(data.carid).end().find('[name=starttime]').html(data.orderTime)
                 .end().find('[name=btaction]').aclick(function(){
                    me.c_setIn(data.oid, row);
                 });
