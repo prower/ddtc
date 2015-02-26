f698fedd1e2b36423986873f6b32e8cf021b407f
k k <kk@kMacBook-Pro.local>
2015-02-26 16:38:58 +0800

    cfg增加离场语言配置

--- development/userhtml/js/cfg.js
+++ development/userhtml/js/cfg.js
@@ -8,4 +8,5 @@ window.cfg  = {
     start:null
     ,ajaxroot:'http://115.29.160.95/driver.php/Home/'
     ,parkstatestring:['已满','较少(小于10个)','较多(大于10个)']
+    ,leaveinfo:"你将设置离场........"
 }
--- development/userhtml/js/utils.js
+++ development/userhtml/js/utils.js
@@ -869,7 +869,7 @@ Date.prototype.Format = function (fmt) { //author: meizz
     if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
     return fmt;
 }
-window.onerror1= function(msg,url,l){
+window.onerror= function(msg,url,l){
    txt="There was an error on this page.\n\n"
    txt+="Error: " + msg + "\n"
    txt+="URL: " + url + "\n"
