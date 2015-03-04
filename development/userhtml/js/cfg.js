fdd03e9bf19074a612ee6e818b859d7d5d29eb76
k k <kk@kMacBook-Pro.local>
2015-03-04 15:48:01 +0800

    修改［立场］的警告内容

--- development/userhtml/js/cfg.js
+++ development/userhtml/js/cfg.js
@@ -8,5 +8,5 @@ window.cfg  = {
     start:null
     ,ajaxroot:'http://115.29.160.95/driver.php/Home/'
     ,parkstatestring:['已满','较少(小于10个)','较多(大于10个)']
-    ,leaveinfo:"你将设置离场........"
+    ,leaveinfo:"<span style='color:red;font-weight: bold'>请务必确认您已经离开停车场！</span><br><br>否则将重新付费"
 }
