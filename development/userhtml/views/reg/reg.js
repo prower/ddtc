d4fa67653e0c610e43769fb6907746c281c90920
joe <hzhou9@gmail.com>
2015-05-05 12:39:02 +0800

    phone number check

--- development/userhtml/views/reg/reg.js
+++ development/userhtml/views/reg/reg.js
@@ -59,6 +59,8 @@ function ui_reg(){
             this.dom.userpanel_chepai.blur();
             if('' == phone){
                 sysmanager.alert('手机号不能为空!');
+            }else if(!(/^1[3|4|5|8][0-9]\d{8}$/.test(phone))){
+            		sysmanager.alert('请输入正确的手机号!');
             }else{
                 sysmanager.login(phone,chepai,function(){
                     me.c_quit();
